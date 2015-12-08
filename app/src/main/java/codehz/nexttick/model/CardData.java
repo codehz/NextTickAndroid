5ebcd8f4a0ab763cc7ff7520e9465c62ae2b4b6a
CodeHz <CodeHz@outlook.com>
2015-12-08 22:23:42 +0800

    add data binding

---
+++ app/src/main/java/codehz/nexttick/model/CardData.java
@@ -0,0 +1,66 @@
+package codehz.nexttick.model;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import java.util.Date;
+
+import codehz.nexttick.BR;
+
+public class CardData extends BaseObservable {
+    private String title;
+    private Date startTime;
+    private Date endTime;
+    private String position;
+    private String description;
+
+    @Bindable
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        notifyPropertyChanged(BR.title);
+    }
+
+    @Bindable
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+        notifyPropertyChanged(BR.startTime);
+    }
+
+    @Bindable
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+        notifyPropertyChanged(BR.endTime);
+    }
+
+    @Bindable
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+        notifyPropertyChanged(BR.endTime);
+    }
+
+    @Bindable
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        notifyPropertyChanged(BR.description);
+    }
+}
--- app/src/main/java/codehz/nexttick/view/Card.java
+++ app/src/main/java/codehz/nexttick/view/Card.java
@@ -1,4 +1,26 @@
 package codehz.nexttick.view;
 
-public class Card {
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+public class Card extends CardView {
+    public Card(Context context) {
+        super(context);
+        initialize(context, null, 0);
+    }
+
+    public Card(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context, attrs, 0);
+    }
+
+    public Card(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize(context, attrs, defStyleAttr);
+    }
+
+    private void initialize(Context context, AttributeSet attrs, int defStyleAttr) {
+
+    }
 }
